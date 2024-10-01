50a113803d3abd35b8825f7fa6e8d72287cdac48
ManhDD <56792079+manh4702@users.noreply.github.com>
2024-10-01 21:22:16 +0700

    changeLayoutFooter

--- src/components/user/Shared/Footer.jsx
+++ src/components/user/Shared/Footer.jsx
@@ -9,13 +9,13 @@ const Footer = () => {
         <div>
           <h3 className="font-bold mb-4">GIỚI THIỆU CHUNG</h3>
           <img
-            src="src/assets/logoKoi.png" // Đặt link logo của bạn ở đây
+            src="src/assets/logoKoi.png"
             alt="KoiFishLogo"
             width="50%"
             height="100"
             className="mb-4"
           />
-          <p>
+          {/* <p>
             Địa chỉ xem cá Koi: Lô E2a-7, Đường D1, Khu Công Nghệ Cao, Long
             Thạnh Mỹ, Hồ Chí Minh.
           </p>
@@ -23,7 +23,7 @@ const Footer = () => {
           <p>Tel: 036988088 - 0907832421 (Zalo)</p>
           <p>Hotline: 036988088 - 0907832421</p>
           <p>Gmail: cakoistore.com</p>
-          <p>Email: cakoistore.com</p>
+          <p>Email: cakoistore.com</p> */}
         </div>
 
         {/* Cột Thông Tin Tài Khoản */}
@@ -53,31 +53,33 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="container mx-auto mt-8">
-        {/* Phần bản đồ và bản quyền */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+      <div className="container mx-auto mb-1">
+        <h3 className="font-bold">BẢN ĐỒ & BẢN QUYỀN</h3>
+        <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-8">
           <div>
-            <h3 className="font-bold">BẢN ĐỒ & BẢN QUYỀN</h3>
+            <p>
+              Địa chỉ xem cá Koi: Lô E2a-7, Đường D1, Khu Công Nghệ Cao, Long
+              Thạnh Mỹ, Hồ Chí Minh.
+            </p>
+            <p>(Ô tô ra vào thoải mái, đỗ xe trực tiếp tại trại 5000m²)</p>
+            <p>Tel: 036988088 - 0907832421 (Zalo)</p>
+            <p>Hotline: 036988088 - 0907832421</p>
+            <p>Gmail: cakoistore.com</p>
+            <p>Email: cakoistore.com</p>
+          </div>
+          <div>
             <iframe
-              src="https://maps.app.goo.gl/dY5y1nsyeox6Za726" // Thay bằng link nhúng Google Maps của bạn
-              width="50%"
-              height="100"
+              src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA4kGszgb6NL-qz4uDEcApxGKQpEXfchzI&q=Lô E2a-7, Đường D1, Đ. D1, Long Thạnh Mỹ, Thành Phố Thủ Đức, Hồ Chí Minh 700000" // Thay bằng link nhúng Google Maps của bạn
+              width="100%"
+              height="100%"
               frameBorder="0"
               allowFullScreen=""
               aria-hidden="false"
               tabIndex="0"
             ></iframe>
           </div>
-
-          <div className="text-center">
-            <p className="mb-4">Nội dung được bảo vệ bởi DMCA</p>
-            <img
-              src="https://your-dmca-link.com/dmca_protected.png" // Thay bằng link ảnh DMCA
-              alt="DMCA Protected"
-            />
-          </div>
         </div>
-        <p className="text-center mt-4">
+        <p className="text-center mt-8">
           Website đang chạy thử nghiệm và chờ cấp phép của BVH - BTTT.
         </p>
         <p className="text-center">
