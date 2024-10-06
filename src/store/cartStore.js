1ab4a1040269016fd3dd46ce77c5534276770b64
ManhDD <56792079+manh4702@users.noreply.github.com>
2024-10-06 21:04:27 +0700

    createCart

---
+++ src/store/cartStore.js
@@ -0,0 +1,29 @@
+import { create } from "zustand";
+
+const useCartStore = create((set) => ({
+  items: [],
+  addItem: (item) =>
+    set((state) => {
+      const existingItem = state.items.find((i) => i.id === item.id);
+      if (existingItem) {
+        return {
+          items: state.items.map((i) =>
+            i.id === item.id ? { ...i, quantity: i.quantity + 1 } : i
+          ),
+        };
+      }
+      return { items: [...state.items, { ...item, quantity: 1 }] };
+    }),
+  removeItem: (id) =>
+    set((state) => ({
+      items: state.items.filter((item) => item.id !== id),
+    })),
+  updateQuantity: (id, quantity) =>
+    set((state) => ({
+      items: state.items.map((item) =>
+        item.id === id ? { ...item, quantity } : item
+      ),
+    })),
+}));
+
+export default useCartStore;
